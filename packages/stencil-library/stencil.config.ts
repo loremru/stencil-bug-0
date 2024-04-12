f62552bd2eb4f200678a7ecb51f4da2b3548098b
loremru <mr.nonamerz08@yandex.ru>
2024-04-12 16:03:49 +0300

    first commit

---
+++ packages/stencil-library/stencil.config.ts
@@ -0,0 +1,31 @@
+import { Config } from '@stencil/core';
+import { vueOutputTarget } from "@stencil/vue-output-target";
+
+export const config: Config = {
+  namespace: 'stencil-library',
+  outputTargets: [
+    {
+      type: 'dist',
+      esmLoaderPath: '../loader',
+    },
+    vueOutputTarget({
+      componentCorePackage: 'stencil-library',
+      proxiesFile: '../vue-library/lib/components.ts',
+    }),
+    {
+      type: 'dist-custom-elements',
+      customElementsExportBehavior: 'auto-define-custom-elements',
+      externalRuntime: false,
+    },
+    {
+      type: 'docs-readme',
+    },
+    {
+      type: 'www',
+      serviceWorker: null, // disable service workers
+    },
+  ],
+  testing: {
+    browserHeadless: "new",
+  },
+};
---
+++ packages/vue-library/dist/components.d.ts
@@ -0,0 +1,1 @@
+export declare const MyComponent: import("vue").DefineSetupFnComponent<any, {}, {}, any, import("vue").PublicProps>;
